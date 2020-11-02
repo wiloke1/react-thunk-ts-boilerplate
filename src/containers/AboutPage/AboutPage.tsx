29eacd9f7dee3e291ce60ee8aa1b0258a6087ab1
wiloke1 <hpvanlong@gmail.com>
2020-11-02 15:39:06 +0700

    delete test about class redux

--- src/containers/AboutPage/AboutPage.tsx
+++ src/containers/AboutPage/AboutPage.tsx
@@ -1,34 +1,11 @@
-import React, { Component } from 'react';
-import { ConnectedProps } from 'react-redux';
-import connector from './connector';
+import React from 'react';
 
-export interface AboutPageProps extends ConnectedProps<typeof connector> {}
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+    </div>
+  );
+};
 
-class AboutPage extends Component<AboutPageProps> {
-  private handleGetTodolist = async () => {
-    const { getTodolistRequest } = this.props;
-    await getTodolistRequest('todolist');
-  };
-
-  public render() {
-    const { todolist } = this.props;
-
-    if (todolist.isLoading) {
-      return <h2>Loading...</h2>;
-    }
-
-    if (!!todolist.errorMessage) {
-      return <h2>{todolist.errorMessage}</h2>;
-    }
-
-    return (
-      <div>
-        <h1>AboutPage</h1>
-        <button onClick={this.handleGetTodolist}>Get todolist</button>
-        <div>{JSON.stringify(todolist.data)}</div>
-      </div>
-    );
-  }
-}
-
-export default connector(AboutPage);
+export default AboutPage;
