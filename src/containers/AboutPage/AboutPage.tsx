de4e2e45caa746fe3b0a09411cde9132c6631244
wiloke1 <hpvanlong@gmail.com>
2020-11-02 15:21:11 +0700

    redux with class component in AboutPage

--- src/containers/AboutPage/AboutPage.tsx
+++ src/containers/AboutPage/AboutPage.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { thunkTodolist } from 'containers/HomePage/thunks/thunkTodolist';
+import { connect, ConnectedProps } from 'react-redux';
 
-const AboutPage = () => {
-  return (
-    <div>
-      <h1>AboutPage</h1>
-    </div>
-  );
+const mapState = (state: AppState) => ({
+  todolist: state.todolist,
+});
+
+const mapDispatch = {
+  getTodolistRequest: thunkTodolist,
 };
 
-export default AboutPage;
+const connector = connect(mapState, mapDispatch);
+
+export interface AboutPageProps extends ConnectedProps<typeof connector> {}
+
+class AboutPage extends Component<AboutPageProps> {
+  private handleGetTodolist = async () => {
+    const { getTodolistRequest } = this.props;
+    await getTodolistRequest('todolist');
+  };
+
+  public render() {
+    const { todolist } = this.props;
+
+    if (todolist.isLoading) {
+      return <h2>Loading...</h2>;
+    }
+
+    if (!!todolist.errorMessage) {
+      return <h2>{todolist.errorMessage}</h2>;
+    }
+
+    return (
+      <div>
+        <h1>AboutPage</h1>
+        <button onClick={this.handleGetTodolist}>Get todolist</button>
+        <div>{JSON.stringify(todolist.data)}</div>
+      </div>
+    );
+  }
+}
+
+export default connector(AboutPage);
