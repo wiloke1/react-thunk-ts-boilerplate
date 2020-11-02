f554beaf0c6548c556286918fdc13fa74c68e182
wiloke1 <hpvanlong@gmail.com>
2020-11-02 23:28:27 +0700

    update fetchAPI

--- src/containers/HomePage/thunks/thunkTodolist.ts
+++ src/containers/HomePage/thunks/thunkTodolist.ts
@@ -9,7 +9,9 @@ type ThunkTodolistAction = ThunkAction<typeof getTodolist>;
 export const thunkTodolist = (endpoint: string): ThunkTodolistAction => async dispatch => {
   dispatch(getTodolist.request(null));
   try {
-    const res: AxiosResponse<TodolistItem[]> = await fetchAPI.get(endpoint);
+    const res: AxiosResponse<TodolistItem[]> = await fetchAPI.request({
+      url: endpoint,
+    });
     dispatch(getTodolist.success(res.data));
   } catch {
     dispatch(getTodolist.failure('Error'));
